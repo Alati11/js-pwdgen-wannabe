e65ecbf0607b6e4cce85fa85e896e7e227032b93
Alati11 <peppealati@gmail.com>
2023-10-03 17:10:32 +0200

    generated secure password

--- js/app.js
+++ js/app.js
@@ -6,18 +6,21 @@ let userName;
 //    - assegnare alla variabile il valore restituito da un prompt
 userName = prompt ('Ciao, qual è il tuo nome?');
 console.log(userName);
+
 // - Chiedi all’utente il suo cognome
 //    - dichiara una variabile
 let userSurname;
 //    - assegnare alla variabile il valore restituito da un prompt
 userSurname = prompt('Qual è il tuo cognome?');
 console.log(userSurname);
+
 // - Chiedi il colore preferito
 //    - dichiara una variabile
 let userColor;
 //    - assegnare alla variabile il valore restituito da un prompt
 userColor = prompt ('Qual è il tuo colore preferito ?');
 console.log(userColor);
+
 // - Creare un numero da associare //
 let userNumber
 // assegna valore //
